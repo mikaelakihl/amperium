69d549fa20e79fa4ea375dd3e01c8a40ebcd0ad5
mikaelakihl <mikaela.kihl@medieinstitutet.se>
2026-07-02 18:48:28 +0200

    refactor: format fix

--- src/components/IconIndicator/IconIndicator.tsx
+++ src/components/IconIndicator/IconIndicator.tsx
@@ -9,7 +9,11 @@ export function IconIndicator({ icon, transparent }: IconIndicatorProps) {
   return (
     <div
       aria-hidden="true"
-      className={transparent ? `${styles.iconIndicator} ${styles.transparent}` : styles.iconIndicator}
+      className={
+        transparent
+          ? `${styles.iconIndicator} ${styles.transparent}`
+          : styles.iconIndicator
+      }
     >
       {icon}
     </div>
