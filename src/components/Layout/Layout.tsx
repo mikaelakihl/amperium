7dd8242b2997f256f74cd3b3f4455ceaafa35765
mikaelakihl <mikaela.kihl@medieinstitutet.se>
2026-07-02 16:57:24 +0200

    refactor: extract navContainer block from layout to a seperate Navigation component for better structure

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,38 +1,16 @@
-import { Navbar } from '../Navbar/Navbar';
-import { LanguageSwitcher } from '../LanguageSwitcher/LanguageSwitcher';
+import { Navigation } from '../Navigation/Navigation';
 import { Link, Outlet } from 'react-router';
 import styles from './Layout.module.css';
-import { NavbarDesktop } from '../NavbarDesktop/NavbarDesktop';
-import { ButtonLink } from '../ButtonLink/ButtonLink';
-import { PhoneOutgoing } from 'lucide-react';
-import { useTranslation } from 'react-i18next';
 import logo from '../../assets/WhiteAmperium24x1024.png';
 
 export function Layout() {
-  const { t } = useTranslation();
   return (
     <div>
       <header className={styles.header}>
         <Link to="/" className={styles.logo}>
           <img src={logo} alt="Amperium" className={styles.logoImage} />
         </Link>
-        <div className={styles.navContainer}>
-          <div className={styles.desktopNav}>
-            <NavbarDesktop />
-          </div>
-          <LanguageSwitcher />
-          <div className={styles.actionButton}>
-            <ButtonLink
-              href="tel:+46 8 854 524"
-              iconLeft={<PhoneOutgoing size={16} />}
-            >
-              {t('nav.callUs')}
-            </ButtonLink>
-          </div>
-          <div className={styles.mobileNav}>
-            <Navbar />
-          </div>
-        </div>
+        <Navigation />
       </header>
       <main>
         <Outlet />
