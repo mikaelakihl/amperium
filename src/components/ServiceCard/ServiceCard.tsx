de9c7cfdd58c3b4c7d354b49a945b2227f7a16f4
mikaelakihl <mikaela.kihl@medieinstitutet.se>
2026-07-02 20:02:35 +0200

    feat: update ServiceCard component to accept dynamic icon prop

--- src/components/ServiceCard/ServiceCard.tsx
+++ src/components/ServiceCard/ServiceCard.tsx
@@ -1,17 +1,18 @@
-import { ArrowRight, Wrench } from 'lucide-react';
+import { ArrowRight } from 'lucide-react';
 import { IconIndicator } from '../IconIndicator/IconIndicator';
 import styles from './ServiceCard.module.css';
 
 interface ServiceCardProps {
   title: string;
   description: string;
+  icon: React.ReactNode;
 }
 
-export function ServiceCard({ title, description }: ServiceCardProps) {
+export function ServiceCard({ title, description, icon }: ServiceCardProps) {
   return (
     <div className={styles.serviceCard}>
       <div className={styles.topContainer}>
-        <IconIndicator icon={<Wrench />} />
+        <IconIndicator icon={icon} />
         <div className={styles.contentContainer}>
           <h3 className="text-title-sm">{title}</h3>
           <p className="text-body-sm">{description}</p>
