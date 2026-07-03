d6070f1ff2661128522083578a4f85d04384fc2c
mikaelakihl <mikaela.kihl@medieinstitutet.se>
2026-07-03 23:17:01 +0200

    feat: update Home component to include Hero and improve layout with a container class that will be used in all index files

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,14 @@
 import { useTranslation } from 'react-i18next';
+import { Hero } from '../../components/Hero/Hero';
 
 export function Home() {
   const { t } = useTranslation();
-  return <h2>{t('home.title')}</h2>;
+  return (
+    <>
+      <Hero />
+      <div className="container">
+        <h2>{t('home.title')}</h2>
+      </div>
+    </>
+  );
 }
