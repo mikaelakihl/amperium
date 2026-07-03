c2a0e3149aa377793bfbe660147e3170a975a8ff
mikaelakihl <mikaela.kihl@medieinstitutet.se>
2026-07-03 17:46:27 +0200

    feat: add nav/link in footer with styling

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,6 +4,47 @@ export function Footer() {
       <Link to="/" className={styles.logo}>
         <img src={logo} alt="Amperium" className={styles.logoImage} />
       </Link>
+      <div className={styles.linkContainer}>
+        <nav className={styles.section} aria-label={t('footer.services')}>
+          <h2 className="text-label-sm">{t('footer.services')}</h2>
+          <ul className={`text-body-sm ${styles.linkList}`}>
+            <li>
+              <Link to="/tjanster/c">Service</Link>
+            </li>
+            <li>
+              <Link to="/tjanster/a">{t('footer.electricalInstallation')}</Link>
+            </li>
+            <li>
+              <Link to="/tjanster/b">{t('footer.projectManagement')}</Link>
+            </li>
+          </ul>
+        </nav>
+        <nav className={styles.section} aria-label={t('footer.theCompany')}>
+          <h2 className="text-label-sm">{t('footer.theCompany')}</h2>
+          <ul className={`text-body-sm ${styles.linkList}`}>
+            <li>
+              <Link to="/about">{t('footer.about')}</Link>
+            </li>
+            <li>
+              <Link to="/contact">{t('footer.contact')}</Link>
+            </li>
+          </ul>
+        </nav>
+        <div className={styles.section} aria-label={t('footer.contact')}>
+          <h2 className="text-label-sm">{t('footer.contact')}</h2>
+          <ul className={`text-body-sm ${styles.linkList}`}>
+            <li>
+              <a href="tel:+46854524">08-854 524</a>
+            </li>
+            <li>
+              <a href="mailto:info@amperium.se">info@amperium.se</a>
+            </li>
+            <li>
+              <address>Klostervägen 3, Gustavsberg</address>
+            </li>
+          </ul>
+        </div>
+      </div>
     </footer>
   );
 }
