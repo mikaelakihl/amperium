85d71fbf17545e77eb797fba40362ace0a0708db
mikaelakihl <mikaela.kihl@medieinstitutet.se>
2026-07-03 23:16:27 +0200

    feat: enhance Hero component with improved layout and translation support

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,5 +1,48 @@
+import { Trans, useTranslation } from 'react-i18next';
+import { Badge } from '../Badge/Badge';
 import styles from './Hero.module.css';
+import { Button } from '../Button/Button';
 
 export function Hero() {
-  return <section className={styles.hero}></section>;
+  const { t } = useTranslation();
+  return (
+    <section className={styles.hero}>
+      <div className={styles.wrapper}>
+        <div className={styles.heroText}>
+          <p className={`${styles.label} text-label-sm`}>
+            {t('hero.electricalInstallation&service')}
+          </p>
+          <p className={`text-heading-md ${styles.slogan}`}>
+            <Trans
+              i18nKey="hero.slogan"
+              components={{
+                highlight: <span className={styles.highlight} />,
+                br: <br />,
+              }}
+            />
+          </p>
+          <div className={styles.buttonsDesktop}>
+            <Button size="xs" fullWidth>
+              {t('hero.contactUs')}
+            </Button>
+            <Button size="xs" variant="secondary" fullWidth>
+              {t('hero.ourServices')}
+            </Button>
+          </div>
+        </div>
+
+        <div className={styles.badgeContainer}>
+          <Badge />
+          <Badge />
+        </div>
+      </div>
+
+      <div className={styles.buttons}>
+        <Button fullWidth>{t('hero.contactUs')}</Button>
+        <Button variant="secondary" fullWidth>
+          {t('hero.ourServices')}
+        </Button>
+      </div>
+    </section>
+  );
 }
