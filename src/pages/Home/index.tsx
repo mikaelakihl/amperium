787293d7b7f4d53d0e6d06af25223511af9d635d
mikaelakihl <mikaela.kihl@medieinstitutet.se>
2026-07-04 20:54:29 +0200

    feat: add responsive BenefitBanner component with different layouts for mobile and desktop

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,6 +3,8 @@ import { Hero } from '../../components/Hero/Hero';
 import { Banner } from '../../components/Banner/Banner';
 import { AboutPreview } from '../../components/AboutPreview/AboutPreview';
 import { ServicePreview } from '../../components/ServicePreview/ServicePreview';
+import { BenefitBanner } from '../../components/BenefitBanner/BenefitBanner';
+import styles from './index.module.css';
 
 export function Home() {
   const { t } = useTranslation();
@@ -13,6 +15,12 @@ export function Home() {
       <div className="container">
         <AboutPreview />
         <ServicePreview />
+        <div className={styles.benefitContainerDesktop}>
+          <BenefitBanner />
+        </div>
+      </div>
+      <div className={styles.benefitContainerMobile}>
+        <BenefitBanner />
       </div>
     </>
   );
