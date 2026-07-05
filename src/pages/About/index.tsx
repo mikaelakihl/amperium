d14b83a95ceda43a7675a7b0c54282bf7863a2e7
mikaelakihl <mikaela.kihl@medieinstitutet.se>
2026-07-05 14:50:05 +0200

    feat: add page intro for about page with translations

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,6 +1,15 @@
 import { useTranslation } from 'react-i18next';
+import { PageIntro } from '../../components/PageIntro/PageIntro';
 
 export function About() {
   const { t } = useTranslation();
-  return <h2>{t('about.title')}</h2>;
+  return (
+    <div className="container">
+      <PageIntro
+        heading={t('about.heading')}
+        description={t('about.description')}
+        note={t('about.note')}
+      />
+    </div>
+  );
 }
