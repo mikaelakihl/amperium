3a6179e730f5774729c153c7437586e47226107a
mikaelakihl <mikaela.kihl@medieinstitutet.se>
2026-07-05 14:49:05 +0200

    feat: add PageIntro component with styles for heading and description

---
+++ src/components/PageIntro/PageIntro.tsx
@@ -0,0 +1,16 @@
+import styles from './PageIntro.module.css';
+
+interface PageIntroProps {
+  heading: string;
+  description: string;
+  note?: string;
+}
+export function PageIntro({ heading, description, note }: PageIntroProps) {
+  return (
+    <div className={styles.pageIntro}>
+      <h2 className={`${styles.heading} text-heading-sm`}>{heading}</h2>
+      <p className={`${styles.description} text-body-md `}>{description}</p>
+      <p className={`${styles.note} text-body-md `}>{note}</p>
+    </div>
+  );
+}
