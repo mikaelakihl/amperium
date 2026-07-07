d144c137769c8eb217fa4cba42691fc693dac094
mikaelakihl <mikaela.kihl@medieinstitutet.se>
2026-07-07 15:42:38 +0200

    feat: implement Form component with styling and translation support

---
+++ src/components/Form/Form.tsx
@@ -0,0 +1,44 @@
+import { useTranslation } from 'react-i18next';
+import { Input } from '../Input/Input';
+import { Textarea } from '../Textarea/Textarea';
+import { Button } from '../Button/Button';
+import { Send, Trash } from 'lucide-react';
+import styles from './Form.module.css';
+
+export function Form() {
+  const { t } = useTranslation();
+
+  return (
+    <form className={styles.form}>
+      <Input
+        label={t('contact.name')}
+        id="name"
+        placeholder={t('contact.name')}
+        required
+      />
+      <Input
+        label={t('contact.epost')}
+        id="epost"
+        placeholder={t('contact.epost')}
+        required
+      />
+      <Textarea
+        label={t('contact.message')}
+        id="message"
+        placeholder={t('contact.message')}
+        required
+      />
+      <Button fullWidth type="submit" iconLeft={<Send />}>
+        {t('contact.requestQuote')}
+      </Button>
+      <Button
+        fullWidth
+        type="reset"
+        iconLeft={<Trash color="var(--color-action-primary)" />}
+        variant="secondary"
+      >
+        {t('contact.clearForm')}
+      </Button>
+    </form>
+  );
+}
