00604117837f87e00ce9857bf17e34fab57f1d49
mikaelakihl <mikaela.kihl@medieinstitutet.se>
2026-07-07 15:42:55 +0200

    feat: add Form in Contact page

--- src/pages/Contact/index.tsx
+++ src/pages/Contact/index.tsx
@@ -1,6 +1,11 @@
 import { useTranslation } from 'react-i18next';
+import { Form } from '../../components/Form/Form';
 
 export function Contact() {
   const { t } = useTranslation();
-  return <h2>{t('contact.title')}</h2>;
+  return (
+    <div className="container">
+      <Form />
+    </div>
+  );
 }
